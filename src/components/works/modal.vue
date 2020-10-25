9e7c62738a8c76140b26d413f5432cacc4d02c4b
orel offir <orel@compie.co.il>
2020-10-25 14:40:49 +0200

    Open modal with form for new work

--- src/components/works/modal.vue
+++ src/components/works/modal.vue
@@ -1,10 +1,169 @@
 <template>
-    $END$
+    <v-row>
+        <v-dialog
+                v-model="dialog.show"
+                persistent
+                max-width="600px"
+        >
+            <template v-slot:activator="{ on, attr }">
+                <v-btn class="mx-sm-4" v-bind="attr" v-on="on">
+                    <v-icon>mdi-plus</v-icon>
+                    <span>הוסף עבודה חדשה</span>
+                </v-btn>
+            </template>
+            <v-card>
+                <v-card-title>
+                    <span class="headline">הוסף עבודה חדשה</span>
+                </v-card-title>
+                <v-card-text>
+                    <v-container>
+                        <v-row>
+                            <v-col cols="12" sm="6" md="4">
+                                <v-text-field
+                                        label="* תאריך"
+                                        v-model="workCard.date"
+                                        :input="dateString(workCard.date)"
+                                        required
+                                ></v-text-field>
+                            </v-col>
+                            <v-col cols="12" sm="6" md="4">
+                                <v-text-field label="איש קשר"
+                                              v-model="workCard.contact"
+                                ></v-text-field>
+                            </v-col>
+                            <v-col cols="12" sm="6" md="4">
+                                <v-select label="חברה"
+                                          :items="['פרטי','יזמות','צבע ארגמן']"
+                                          v-model="workCard.companyType"
+                                          :input="workCard.companyType"
+                                          persistent-hint
+                                          required
+                                ></v-select>
+                            </v-col>
+                            <v-col cols="12" sm="6" md="4">
+                                <v-text-field label="* לוחית רישוי"
+                                              v-model="workCard.licensePlate"
+                                              required
+                                ></v-text-field>
+                            </v-col>
+                            <v-col cols="12" sm="6" md="4">
+                                <v-text-field label="שעות עבודה"
+                                              type="number"
+                                              v-model="workCard.workTime"
+                                ></v-text-field>
+                            </v-col>
+                            <v-col cols="12" sm="12" md="12">
+                                <v-text-field label="תיאור"
+                                              hint="תיקון, טיפול, חילוץ"
+                                              v-model="workCard.description"
+                                              persistent-hint
+                                              required
+                                ></v-text-field>
+                            </v-col>
+                            <v-col cols="12" sm="6" md="4">
+                                <v-text-field label="* מחיר עבודה"
+                                              type="number"
+                                              v-model="workCard.workPrice"
+                                              required
+                                ></v-text-field>
+                            </v-col>
+                            <v-col cols="12" sm="6" md="4">
+                                <v-text-field label="* מחיר חלקים"
+                                              type="number"
+                                              v-model="workCard.partsCost"
+                                ></v-text-field>
+                            </v-col>
+                            <v-col cols="12" sm="6" md="4">
+                                <v-text-field label="הוצאות"
+                                              type="number"
+                                              hint="עלות החלקים כולל מע''מ"
+                                              v-model="workCard.partsPrice"
+                                              persistent-hint
+                                              required
+                                ></v-text-field>
+                            </v-col>
+                            <v-col cols="12" sm="1">
+                                <v-checkbox v-model="workCard.paid"
+                                ></v-checkbox>
+                            </v-col>
+                            <v-col cols="12" sm="5">
+                                <v-text-field label="* שולם"
+                                              v-model="workCard.paidSum"
+                                              required
+                                ></v-text-field>
+                            </v-col>
+                            <v-col cols="12" sm="6">
+                                <v-autocomplete label="אמצעי תשלום"
+                                                single
+                                                v-model="workCard.paymentInstrument"
+                                                :items="['מזומן', 'צק', 'העברה', 'אשראי']"
+                                ></v-autocomplete>
+                            </v-col>
+                            <v-col cols="12">
+                                <v-text-field label="הערות"
+                                              v-model="workCard.notes"
+                                ></v-text-field>
+                            </v-col>
+                        </v-row>
+                    </v-container>
+                    <small>* שדות חובה</small>
+                </v-card-text>
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn
+                            color="blue darken-1"
+                            text
+                            @click="close"
+                    >
+                        Close
+                    </v-btn>
+                    <v-btn
+                            color="blue darken-1"
+                            text
+                            @click="save"
+                    >
+                        Save
+                    </v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
+    </v-row>
 </template>
 
 <script>
     export default {
-        name : "modal"
+        name : "modal",
+        props : {
+            dialog : {
+                type : Object,
+                default() {
+                    return {
+                        show : false,
+                        data : {}
+                    }
+                }
+            },
+        },
+        data() {
+            return {}
+        },
+        computed : {
+            workCard() {
+                return this.dialog.data.workCard || {};
+            }
+        },
+        methods : {
+            save() {
+                this.$emit('save', this.workCard);
+                this.close();
+            },
+            close() {
+                this.dialog.show = false;
+            },
+            dateString(timestamp) {
+                return new Date(timestamp).toLocaleDateString();
+            }
+        }
     }
 </script>
 
--- src/components/works/table.vue
+++ src/components/works/table.vue
@@ -1,10 +1,110 @@
 <template>
-    $END$
+    <div class="worksTable">
+        <v-data-table
+                item-key="id"
+                class="elevation-1"
+                :items="works"
+                :headers="headers"
+                :search="search"
+                :custom-filter="searchFilter"
+        >
+            <template v-slot:top>
+                <v-text-field
+                        v-model="search"
+                        label="חיפוש"
+                        class="mx-4"
+                ></v-text-field>
+            </template>
+            <template v-slot:item.date="{ item }">
+                <span>{{ dateString(item.date) }}</span>
+            </template>
+            <template v-slot:item.actions="{ item }">
+                <v-icon @click="edit(item)">mdi-briefcase-edit</v-icon>
+                <v-icon @click="edit(item)">mdi-delete-circle</v-icon>
+            </template>
+            <template v-slot:body.append>
+                <tr>
+                    <td colspan="1">
+                        <v-text-field
+                                v-model="month"
+                                type="string"
+                                label="חודש"
+                        ></v-text-field>
+                    </td>
+                </tr>
+            </template>
+        </v-data-table>
+    </div>
 </template>
 
 <script>
     export default {
-        name : "table"
+        name : "worksTable",
+        props: {
+            works : {
+                type: Array,
+                default: function () {
+                    return [];
+                }
+            }
+        },
+        data() {
+            return {
+                search : '',
+                month : 0,
+            }
+        },
+        computed : {
+            headers() {
+                return [
+                    {
+                        text : 'תאריך',
+                        align : 'start',
+                        value : 'date',
+                        dataType: 'Date',
+                        width: 100,
+                        filter : value => {
+                            if (!parseInt(this.month)) return true;
+
+                            return new Date(value).getMonth() + 1  === parseInt(this.month);
+                        },
+                    },
+                    {
+                        text : 'איש קשר',
+                        value : 'contact',
+                    },
+                    { text : 'לקוח', value : 'companyType' },
+                    { text : 'לוחית רישוי', value : 'licensePlate' },
+                    { text : 'זמן עבודה', value : 'workTime' },
+                    { text : 'תיאור עבודה', value : 'description' },
+                    { text : 'מחיר עבודה', value : 'workPrice' },
+                    { text : 'מחיר חלקים', value : 'partsPrice' },
+                    { text : 'הוצאות', value : 'partsCost' },
+                    { text : 'שולם', value : 'paid' },
+                    { text : 'אמצעי תשלום', value : 'paymentInstrument' },
+                    { text : 'הערות', value : 'notes', sortable : false },
+                    { text : 'פעולות', value: 'actions', sortable : false, width: 100 },
+                ]
+            },
+        },
+        methods : {
+            searchFilter(value, search, item) {
+                return value != null && search != null &&
+                    value.toString().toLowerCase().indexOf(search.toLowerCase()) !== -1
+            },
+            dateString(timestamp){
+                return new Date(timestamp).toLocaleDateString();
+            },
+            edit(item){
+                this.$emit('edit', item);
+            },
+            filterOnlyCapsText(value, search, item) {
+                return value != null &&
+                    search != null &&
+                    typeof value === 'string' &&
+                    value.toString().toLocaleUpperCase().indexOf(search) !== -1
+            },
+        }
     }
 </script>
 
